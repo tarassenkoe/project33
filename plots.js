dd0766cc845bbbf2f8af7296aad39a3ea7eb1ece
tarassenkoe <73853172+tarassenkoe@users.noreply.github.com>
2023-10-23 19:13:01 -0400

    Update plots.js

--- plots.js
+++ plots.js
@@ -23,20 +23,20 @@ for (let i=0;i<Results.length;i++) {
   Outcomes.push(Outcome);
   let trace1={x:Glucoses, y: Outcomes, text:"Glucose Levels", name:"Glucose",type:"bar"};
   let data1=[trace1];
-  let layout={title:"Glucose Levels Vs. Outcome",barmode:"group",margin:{l:50,r:50,b:200,t:50,pad:4}};
-  Plotly.newplot("plot",data1,layout);
+  let layout1={title:"Glucose Levels Vs. Outcome",barmode:"group",margin:{l:50,r:50,b:200,t:50,pad:4}};
+  Plotly.newplot("plot",data1,layout1);
 
   let trace2={x:BloodPressures, y: Outcomes, text:"BloodPressure Levels", name:"BloodPressure", type:"bar"};
   let data2=[trace2];
-  let layout={title:"BloodPressure Levels Vs. Outcome",barmode:"group",margin:{l:50,r:50,b:200,t:50,pad:4}};
-  Plotly.newplot("plot",data2,layout);
+  let layout2={title:"BloodPressure Levels Vs. Outcome",barmode:"group",margin:{l:50,r:50,b:200,t:50,pad:4}};
+  Plotly.newplot("plot",data2,layout2);
 
   let trace3={x:Ages, y: Outcomes, text:"Ages", name:"Age",type:"bar"};
   let data3=[trace3];
-  let layout={title:"Ages Vs. Outcome",barmode:"group",margin:{l:50,r:50,b:200,t:50,pad:4}};
-  Plotly.newplot("plot",data3,layout);
+  let layout3={title:"Ages Vs. Outcome",barmode:"group",margin:{l:50,r:50,b:200,t:50,pad:4}};
+  Plotly.newplot("plot",data3,layout3);
 
   let trace4={x:BMIs, y: Outcomes, text:"BMIs", name:"BMI",type:"bar"};
   let data4=[trace4];
-  let layout={title:"BMIs Vs. Outcome",barmode:"group",margin:{l:50,r:50,b:200,t:50,pad:4}};
-  Plotly.newplot("plot",data4,layout);
+  let layout4={title:"BMIs Vs. Outcome",barmode:"group",margin:{l:50,r:50,b:200,t:50,pad:4}};
+  Plotly.newplot("plot",data4,layout4);
